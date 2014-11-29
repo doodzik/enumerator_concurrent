071b64c12a59d809358f7aaba0a96cc2caf54317
doodzik <frederik.dudzik@gmail.com>
2014-11-29 15:37:04 +0100

    add basic functionalety

--- lib/Enumerator/Concurrent.rb
+++ lib/Enumerator/Concurrent.rb
@@ -1,5 +1,26 @@
 require "Enumerator/Concurrent/version"
 
 class Enumerator::Concurrent
-    # Your code goes here...
+  attr_reader :list
+  def initialize(list)
+    @list = list
+  end
+
+  def each(&block)
+    setup(&block).join.list
+  end
+
+  def join
+    Enumerator::Concurrent.new @list.map { |x| x.join.value }
+  end
+
+  private
+
+  def setup
+    threads = @list.map do |x|
+      Thread.new { yield(x) }
+    end
+    Enumerator::Concurrent.new threads
+  end
+
 end
--- test/test_enumerator-concurrent.rb
+++ test/test_enumerator-concurrent.rb
@@ -1,5 +1,51 @@
 require 'minitest/autorun'
+require 'enumerator/concurrent'
 
-class EnumeratorConcurrent < Minitest::Test
+class ThreadMock
+  attr_reader :joined, :valued
+  @joined = false
+  @value = false
 
+  def join
+    @joined = true
+    self
+  end
+
+  def value
+    @valued = true
+    self
+  end
+end
+
+class EnumeratorConcurrentTest < Minitest::Test
+  def test_creates_list_of_threads
+    thread1, thread2 = Enumerator::Concurrent.new([2,3]).send(:setup) do |x|
+      x + 10
+    end.list
+
+    assert(thread1.is_a? Thread)
+    assert(thread2.is_a? Thread)
+    assert_equal(thread1.value, 12)
+    assert_equal(thread2.value, 13)
+  end
+
+  def test_joining_threads
+    threads = [ ThreadMock.new, ThreadMock.new ]
+    t1, t2 = Enumerator::Concurrent.new(threads).join.list
+    assert(t1.joined)
+    assert(t1.valued)
+    assert(t2.joined)
+    assert(t2.valued)
+  end
+
+  def test_each
+    values = [ 3, 2 ]
+    ec = Enumerator::Concurrent.new(values)
+    t1, t2 = ec.each { |x| x + 2 }
+    assert_equal(t1, 5)
+    assert_equal(t2, 4)
+  end
+
+  # def test_extended_enumerator
+  # end
 end
